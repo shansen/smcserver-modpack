36e6c0c7694a6f67923f07fbba645c991efbc202
Shansen <me@shansen.me>
2014-05-26 00:34:24 +0100

    .

--- build_client.sh
+++ build_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find . -name ".DS_Store" -exec rm -f '{}' +
-java -cp 	../launcher/target/ShanCraftLauncher.jar \
+java -cp 	../launcher/target/SmcServerLauncher.jar \
 			com.skcraft.launcher.builder.PackageBuilder \
 			--manifest-dest "client/manifest.json" \
 			--objects-dest "client/objects" \
